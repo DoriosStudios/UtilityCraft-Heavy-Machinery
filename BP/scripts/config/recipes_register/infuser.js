105a46081d6889390b1fe206e42cfc806744cc70
Dorios Studios <chavez100577@gmail.com>
2025-12-10 15:03:34 -0600

    Added for Power Condenser Stuff

--- BP/scripts/config/recipes_register/infuser.js
+++ BP/scripts/config/recipes_register/infuser.js
@@ -1,7 +1,11 @@
 import { system, world } from "@minecraft/server";
 
 const newRecipes = {
+    // Steel
     "utilitycraft:steel_dust|minecraft:glass": { output: "utilitycraft:tempered_steel_glass", required: 8 },
+    // Darloonite
+    "minecraft:echo_shard|minecraft:amethyst_shard": { output: "utilitycraft:darloonite_crystal", required: 1, cost: 3200 },
+    "utilitycraft:diamond_dust|utilitycraft:darloonite_crystal": { output: "utilitycraft:charged_darloonite_crystal", required: 8, cost: 128_000 },
 };
 
 world.afterEvents.worldLoad.subscribe(() => {
