4366e8f8623811b3676afe448f61fc6cca2ab3db
Dorios Studios <chavez100577@gmail.com>
2025-12-10 16:19:18 -0600

    Added more recipes

--- BP/scripts/config/recipes_register/infuser.js
+++ BP/scripts/config/recipes_register/infuser.js
@@ -4,7 +4,7 @@ const newRecipes = {
     // Steel
     "utilitycraft:steel_dust|minecraft:glass": { output: "utilitycraft:tempered_steel_glass", required: 8 },
     // Darloonite
-    "minecraft:echo_shard|minecraft:amethyst_shard": { output: "utilitycraft:darloonite_crystal", required: 1, cost: 3200 },
+    "minecraft:echo_shard|minecraft:amethyst_shard": { output: "utilitycraft:darloonite_crystal", required: 1, cost: 6400 },
     "utilitycraft:diamond_dust|utilitycraft:darloonite_crystal": { output: "utilitycraft:charged_darloonite_crystal", required: 8, cost: 128_000 },
 };
 
