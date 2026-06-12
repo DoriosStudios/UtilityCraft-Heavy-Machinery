e3420365427c40b67691293ca05160bf8173d947
Dorios Studios <chavez100577@gmail.com>
2026-06-12 13:28:29 -0600

    Added inanimate family to entities

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,8 +1,5 @@
-import './DoriosAPI/index.js'
-import './DoriosCore/index.js'
-import './machinery/main.js'
+import "./DoriosAPI/index.js";
+import "./DoriosCore/index.js";
+import "./machinery/main.js";
 
-import './config/main.js'
-import './insight_injectors.js'
-import './insight_registry_injector.generated.js'
-
+import "./config/main.js";
