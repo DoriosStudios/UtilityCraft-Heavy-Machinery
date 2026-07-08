c7e9c6ea4e1b4a26c0afd1cc6d7f7a7426ed83a4
Milo504 <chavez100577@gmail.com>
2026-07-07 21:31:46 -0600

    Recipes UI Register

--- BP/scripts/config/recipes_register/crusher.js
+++ BP/scripts/config/recipes_register/crusher.js
@@ -8,16 +8,19 @@ const newRecipes = {
     "utilitycraft:tin_ingot": { output: "utilitycraft:tin_dust", amount: 1 },
     "utilitycraft:tin_plate": { output: "utilitycraft:tin_dust", amount: 1 },
     "utilitycraft:raw_tin_block": { output: "utilitycraft:tin_dust", amount: 12 },
+    "utilitycraft:tin_block": { output: "utilitycraft:tin_dust", amount: 6 },
     // Uranium
     "utilitycraft:deepslate_uranium_chunk": { output: "utilitycraft:raw_uranium", amount: 1 },
     "utilitycraft:raw_uranium": { output: "utilitycraft:uranium_dust", amount: 2 },
     "utilitycraft:uranium_ingot": { output: "utilitycraft:uranium_dust", amount: 1 },
     "utilitycraft:raw_uranium_block": { output: "utilitycraft:uranium_dust", amount: 12 },
+    "utilitycraft:uranium_block": { output: "utilitycraft:uranium_dust", amount: 6 },
     // Bronze
     "utilitycraft:brute_bronze": { output: "utilitycraft:bronze_dust", amount: 2 },
     "utilitycraft:bronze_ingot": { output: "utilitycraft:bronze_dust", amount: 1 },
     "utilitycraft:bronze_plate": { output: "utilitycraft:bronze_dust", amount: 1 },
     "utilitycraft:brute_bronze_block": { output: "utilitycraft:bronze_dust", amount: 12 },
+    "utilitycraft:bronze_block": { output: "utilitycraft:bronze_dust", amount: 6 },
 };
 
 world.afterEvents.worldLoad.subscribe(() => {
--- BP/scripts/config/recipes_register/press.js
+++ BP/scripts/config/recipes_register/press.js
@@ -2,11 +2,11 @@ import { system, world } from "@minecraft/server";
 
 const newRecipes = {
     // Tin
-    "utilitycraft:tin_ingot": { output: "utilitycraft:tin_plate", amount: 1 },
+    "utilitycraft:tin_ingot": { output: "utilitycraft:tin_plate", required: 1 },
     // Uranium
-    "utilitycraft:uranium_ingot": { output: "utilitycraft:uranium_rod", amount: 1 },
+    "utilitycraft:uranium_ingot": { output: "utilitycraft:uranium_rod", required: 1 },
     // Bronze
-    "utilitycraft:bronze_ingot": { output: "utilitycraft:bronze_plate", amount: 1 },
+    "utilitycraft:bronze_ingot": { output: "utilitycraft:bronze_plate", required: 1 },
 };
 
 world.afterEvents.worldLoad.subscribe(() => {
